c954e10d7c45a7398f8a27c1e35d92e304027e0e
harpCS <100532126+upcaSSSt@users.noreply.github.com>
2024-08-29 21:14:03 +0300

    Update long-date.js

--- src/long-date.js
+++ src/long-date.js
@@ -9,12 +9,13 @@ export default class LongDate extends CountdownDate {
   
   echo(sep = ': ', style = '**') {
     const now = new Date();
-    const years = new Date(this.shiftTimezone() - now).getFullYear() - 1970;
-    const sec = this.secToDate();
+    const years = new Date(this.secToDate()).getFullYear() - 1970;
+    const sec = Math.trunc(this.secToDate() / 1000);
     const hours = this._rounder(sec / 3600);
     const min = this._rounder(sec / 60);
-    const d2 = new Date(msOffset(new Date(this._date.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes()), this._userOffset));
-
+    // Если дата прошла в этом году, то ставим следующий год, иначе текущий
+    const d2 = new Date(msOffset(new Date(now.getFullYear() + 1, this._date.getMonth(), this._date.getDate(), this._date.getHours(), this._date.getMinutes()), this._userOffset));
+    
     return `${style}${this._name}${style}${sep}${years}г ${this._rounder((this._date - d2) / 86_400_000)}дн ${hours % 24}ч ${min % 60}мин ${sec % 60}сек\n`
       + `(${this._rounder(sec / 86_400).toLocaleString('ru')}дн ${hours.toLocaleString('ru-RU')}ч ${min.toLocaleString('ru-RU')}мин ${sec.toLocaleString('ru-RU')}сек)`;
   }

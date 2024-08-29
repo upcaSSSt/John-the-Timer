9974a86932ed249582a4c6ade80082b000f173c9
harpCS <100532126+upcaSSSt@users.noreply.github.com>
2024-08-29 21:14:32 +0300

    Update long-date.js

--- src/long-date.js
+++ src/long-date.js
@@ -13,7 +13,7 @@ export default class LongDate extends CountdownDate {
     const sec = Math.trunc(this.secToDate() / 1000);
     const hours = this._rounder(sec / 3600);
     const min = this._rounder(sec / 60);
-    // Если дата прошла в этом году, то ставим следующий год, иначе текущий
+    // Если дата была в этом году, то ставим следующий год, иначе текущий
     const d2 = new Date(msOffset(new Date(now.getFullYear() + 1, this._date.getMonth(), this._date.getDate(), this._date.getHours(), this._date.getMinutes()), this._userOffset));
     
     return `${style}${this._name}${style}${sep}${years}г ${this._rounder((this._date - d2) / 86_400_000)}дн ${hours % 24}ч ${min % 60}мин ${sec % 60}сек\n`
